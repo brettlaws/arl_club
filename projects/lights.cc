f18e4e891030c7c7c422f6c100491ee942b22f4d
brettlaws <brettlaws94@gmail.com>
2021-10-25 08:01:37 -0600

    Fixed lights.cc

--- projects/lights.cc
+++ projects/lights.cc
@@ -1,63 +1,59 @@
-const int sensorPin = 0;
-const int ledPin = 9;
+const int sensorPin = A5;
+const int ledPin = 13;
 
 int sensorCalibration = 0;
-int roomLightThreshold = 0; // Unknown
+int roomLightThreshold = 440; // Unknown
 int sensorValue = 0;
 
-void calibrateSensor()
-{
-    sensorCalibration = analogRead(sensorPin) * -1;
+void calibrateSensor() {
+  sensorCalibration = (analogRead(sensorPin) * -1) + 20;
 }
 
-int readSensor()
-{
-    int sensorValue = analogRead(sensorPin) + sensorCalibration;
-    Serial.print("Sensor Value: ");
-    Serial.println(sensorValue);
-    return sensorValue;
+int readSensor() {
+  int sensorValue =   analogRead(sensorPin) + sensorCalibration;
+  Serial.print("Sensor Value: ");
+  Serial.println(sensorValue);
+  return sensorValue;
 }
 
-bool isRoomLight(int sensorValue)
-{
-    if (sensorValue < roomLightThreshold)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+bool isRoomLight(int sensorValue) {
+  if (sensorValue < roomLightThreshold)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 }
 
-void ledOff()
-{
-    digitalWrite(ledPin, LOW);
+void ledOff() {
+  digitalWrite(ledPin, LOW);
 }
 
-void ledOn()
-{
-    digitalWrite(ledPin, HIGH);
+void ledOn() {
+  digitalWrite(ledPin, HIGH);
 }
 
 void setup()
 {
-    pinMode(ledPin, OUTPUT);
-    digitalWrite(ledPin, LOW);
-
-    calibrateSensor();
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+  
+  calibrateSensor();
 }
 
 void loop()
 {
-    sensorValue = readSensor();
+  sensorValue = readSensor();
 
-    if (isRoomLight(sensorValue) == true)
-    {
-        ledOff();
-    }
-    else
-    {
-        ledOn();
-    }
+  if (isRoomLight(sensorValue) == true)
+  {
+    ledOff();
+  }
+  else
+  {
+    ledOn();
+  }
 }
